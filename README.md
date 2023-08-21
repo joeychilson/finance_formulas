a04f9dd8e8c2ea13722281afddfd4e675190fff6
Joey Chilson <joeychilson@outlook.com>
2023-08-20 22:19:34 -0500

    Updated for v0.1.0 release

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # FinancialFormulas
 
 An Elixir library for financial formulas.
+
+## Installation
+
+`FinancialFormulas` is available on Hex. Add it to your list of dependencies in mix.exs:
+
+```elixir
+def deps do
+  [
+    {:financial_formulas, "~> 0.1.0"}
+  ]
+end
+```
--- mix.exs
+++ mix.exs
@@ -1,28 +1,38 @@
 defmodule FinancialFormulas.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "An Elixir library for financial formulas."
+
   def project do
     [
       app: :financial_formulas,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: @description,
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/joeychilson/financial_formulas"},
+      maintainers: ["Joey Chilson"]
     ]
   end
 end
